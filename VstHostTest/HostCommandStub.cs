d9c412fdb91e87f997cf188d9a4ec4916cec2779
Dalgona <dalgona@hontou.moe>
2018-07-08 00:36:55 +0900

    Move host command stub code

---
+++ VstHostTest/HostCommandStub.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Jacobi.Vst.Core;
+using Jacobi.Vst.Core.Host;
+
+namespace VstHostTest
+{
+    class HostCommandStub : IVstHostCommandStub
+    {
+        public IVstPluginContext PluginContext { get; set; }
+
+        public bool BeginEdit(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VstCanDoResult CanDo(string cando)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CloseFileSelector(VstFileSelect fileSelect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EndEdit(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VstAutomationStates GetAutomationState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBlockSize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCurrentPluginID()
+        {
+            return PluginContext.PluginInfo.PluginID;
+        }
+
+        public string GetDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetInputLatency()
+        {
+            throw new NotImplementedException();
+        }
+
+        public VstHostLanguage GetLanguage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetOutputLatency()
+        {
+            throw new NotImplementedException();
+        }
+
+        public VstProcessLevels GetProcessLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProductString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetSampleRate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public VstTimeInfo GetTimeInfo(VstTimeInfoFlags filterFlags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetVendorString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetVendorVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetVersion()
+        {
+            return 1000;
+        }
+
+        public bool IoChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenFileSelector(VstFileSelect fileSelect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProcessEvents(VstEvent[] events)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessIdle()
+        {
+        }
+
+        public void SetParameterAutomated(int index, float value)
+        {
+        }
+
+        public bool SizeWindow(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDisplay()
+        {
+            return false;
+        }
+    }
+}
--- VstHostTest/MainViewModel.cs
+++ VstHostTest/MainViewModel.cs
@@ -8,8 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Input;
-using Jacobi.Vst.Core;
-using Jacobi.Vst.Core.Host;
 using Jacobi.Vst.Interop.Host;
 
 namespace VstHostTest
@@ -54,7 +52,7 @@ namespace VstHostTest
         {
             try
             {
-                NullHostCommandStub hostCmdStub = new NullHostCommandStub();
+                HostCommandStub hostCmdStub = new HostCommandStub();
                 VstPluginContext ctx = VstPluginContext.Create(pluginPath, hostCmdStub);
 
                 ctx.Set("PluginPath", pluginPath);
@@ -94,134 +92,6 @@ namespace VstHostTest
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return null;
-        }
-    }
-
-    class NullHostCommandStub : IVstHostCommandStub
-    {
-        public IVstPluginContext PluginContext { get; set; }
-
-        public bool BeginEdit(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public VstCanDoResult CanDo(string cando)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool CloseFileSelector(VstFileSelect fileSelect)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool EndEdit(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public VstAutomationStates GetAutomationState()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetBlockSize()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetCurrentPluginID()
-        {
-            return PluginContext.PluginInfo.PluginID;
-        }
-
-        public string GetDirectory()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetInputLatency()
-        {
-            throw new NotImplementedException();
-        }
-
-        public VstHostLanguage GetLanguage()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetOutputLatency()
-        {
-            throw new NotImplementedException();
-        }
-
-        public VstProcessLevels GetProcessLevel()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetProductString()
-        {
-            throw new NotImplementedException();
-        }
-
-        public float GetSampleRate()
-        {
-            throw new NotImplementedException();
-        }
-
-        public VstTimeInfo GetTimeInfo(VstTimeInfoFlags filterFlags)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetVendorString()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetVendorVersion()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetVersion()
-        {
-            return 1000;
-        }
-
-        public bool IoChanged()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool OpenFileSelector(VstFileSelect fileSelect)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool ProcessEvents(VstEvent[] events)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ProcessIdle()
-        {
-        }
-
-        public void SetParameterAutomated(int index, float value)
-        {
-        }
-
-        public bool SizeWindow(int width, int height)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool UpdateDisplay()
-        {
-            return false;
         }
     }
 }
