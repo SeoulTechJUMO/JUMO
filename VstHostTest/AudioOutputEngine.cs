50cdbc5a240d14fa13ccbd0b49b55db41f951210
Dalgona <dalgona@hontou.moe>
2018-07-08 18:22:46 +0900

    Add AudioOutputEngine

---
+++ VstHostTest/AudioOutputEngine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace VstHostTest
+{
+    class AudioOutputEngine : IDisposable
+    {
+        private const int SAMPLE_RATE = 44100;
+        private const int NUM_CHANNEL = 2;
+
+        private readonly IWavePlayer outputDevice;
+        private readonly MixingSampleProvider mixer;
+
+        public AudioOutputEngine(IAudioOutputDevice device)
+        {
+            switch (device.Type)
+            {
+                case AudioOutputDeviceType.WaveOut:
+                    outputDevice = new WaveOut() { DeviceNumber = (int)device.Identifier };
+                    break;
+                case AudioOutputDeviceType.DirectSound:
+                    outputDevice = new DirectSoundOut((Guid)device.Identifier);
+                    break;
+                case AudioOutputDeviceType.ASIO:
+                    outputDevice = new AsioOut((string)device.Identifier);
+                    break;
+            }
+
+            mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(SAMPLE_RATE, NUM_CHANNEL))
+            {
+                ReadFully = true
+            };
+
+            outputDevice.Init(mixer);
+            outputDevice.Play();
+        }
+
+        // WIP
+        public void AddMixerInput(ISampleProvider input)
+        {
+            mixer.AddMixerInput(input);
+        }
+
+        public void Dispose()
+        {
+            outputDevice.Stop();
+            outputDevice.Dispose();
+        }
+    }
+}
