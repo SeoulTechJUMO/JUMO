16bb5edbe473c452d31786daafde28671a352994
Dalgona <dalgona@hontou.moe>
2018-07-08 16:19:55 +0900

    [WIP] Add AudioOutputDevice and AudioManager

---
+++ VstHostTest/AudioOutputDevice.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace VstHostTest
+{
+    enum AudioOutputDeviceType
+    {
+        WaveOut,
+        DirectSound,
+        ASIO
+    }
+
+    interface IAudioOutputDevice
+    {
+        AudioOutputDeviceType Type { get; }
+        string FriendlyName { get; }
+        object Identifier { get; }
+    }
+
+    class WaveOutDevice : IAudioOutputDevice
+    {
+        public AudioOutputDeviceType Type => AudioOutputDeviceType.WaveOut;
+        public string FriendlyName { get; private set; }
+        public object Identifier { get; private set; }
+
+        public WaveOutDevice(int id)
+        {
+            Identifier = id;
+            FriendlyName = WaveOut.GetCapabilities(id).ProductName;
+        }
+
+        public override string ToString() => $"{Type}: {FriendlyName}";
+    }
+
+    class DirectSoundOutputDevice : IAudioOutputDevice
+    {
+        public AudioOutputDeviceType Type => AudioOutputDeviceType.DirectSound;
+        public string FriendlyName { get; private set; }
+        public object Identifier { get; private set; }
+
+        public DirectSoundOutputDevice(Guid guid, string description)
+        {
+            FriendlyName = description;
+            Identifier = guid;
+        }
+
+        public override string ToString() => $"{Type}: {FriendlyName}";
+    }
+
+    class AsioOutputDevice : IAudioOutputDevice
+    {
+        public AudioOutputDeviceType Type => AudioOutputDeviceType.ASIO;
+        public string FriendlyName { get; private set; }
+        public object Identifier => FriendlyName;
+
+        public AsioOutputDevice(string driverName)
+        {
+            FriendlyName = driverName;
+        }
+
+        public override string ToString() => $"{Type}: {FriendlyName}";
+    }
+}
---
+++ vsthosttest/AudioManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using NAudio.Wave;
+
+namespace VstHostTest
+{
+    sealed class AudioManager
+    {
+        #region Singleton
+
+        private static readonly Lazy<AudioManager> _instance = new Lazy<AudioManager>(() => new AudioManager());
+        private AudioManager()
+        {
+            PopulateAudioOutputDevices();
+        }
+        public static AudioManager Instance => _instance.Value;
+
+        #endregion
+
+        private ObservableCollection<IAudioOutputDevice> _outputDevices;
+
+        public ICollectionView OutputDevices { get; private set; }
+        public IAudioOutputDevice SelectedOutputDevice { get; set; } = null;
+
+        private void PopulateAudioOutputDevices()
+        {
+            if (_outputDevices == null)
+            {
+                _outputDevices = new ObservableCollection<IAudioOutputDevice>();
+            }
+
+            int numOfWaveOutDevices = WaveOut.DeviceCount;
+            for (int i = 0; i < numOfWaveOutDevices; i++)
+            {
+                _outputDevices.Add(new WaveOutDevice(i));
+            }
+
+            foreach (var dsDev in DirectSoundOut.Devices)
+            {
+                _outputDevices.Add(new DirectSoundOutputDevice(dsDev.Guid, dsDev.Description));
+            }
+
+            foreach (var asio in AsioOut.GetDriverNames())
+            {
+                _outputDevices.Add(new AsioOutputDevice(asio));
+            }
+
+            OutputDevices = CollectionViewSource.GetDefaultView(_outputDevices);
+        }
+    }
+}
