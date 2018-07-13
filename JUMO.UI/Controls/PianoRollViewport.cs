ae471ded3bb7f17a12e54b19594a9f83e64c5898
Dalgona <dalgona@hontou.moe>
2018-07-13 09:23:17 +0900

    PianoRollViewport 클래스 생성

---
+++ JUMO.UI/Controls/PianoRollViewport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace JUMO.UI.Controls
+{
+    class PianoRollViewport : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            System.Diagnostics.Debug.WriteLine($"Actual: {ActualWidth} x {ActualHeight}");
+            System.Diagnostics.Debug.WriteLine($"RenderSize: {RenderSize}");
+            dc.DrawRectangle(new SolidColorBrush(Color.FromRgb(255, 0, 0)), null, new Rect(20, 20, 80, 60));
+        }
+    }
+}
