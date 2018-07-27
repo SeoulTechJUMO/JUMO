22ecefaefc550e5d6ffd288923dcb6752bf24767
Dalgona <dalgona@hontou.moe>
2018-07-27 21:43:05 +0900

    MusicalGrid에서 자체 DP 대신 Attached Property 사용

--- JUMO.UI/Controls/MusicalGrid.cs
+++ JUMO.UI/Controls/MusicalGrid.cs
@@ -13,56 +13,11 @@ namespace JUMO.UI.Controls
     {
         #region Dependency Properties
 
-        public static readonly DependencyProperty NumeratorProperty =
-            DependencyProperty.Register(
-                "Numerator", typeof(int), typeof(MusicalGrid),
-                new FrameworkPropertyMetadata(
-                    4,
-                    FrameworkPropertyMetadataOptions.AffectsRender
-                )
-            );
-
-        public static readonly DependencyProperty DenominatorProperty =
-            DependencyProperty.Register(
-                "Denominator", typeof(int), typeof(MusicalGrid),
-                new FrameworkPropertyMetadata(
-                    4,
-                    FrameworkPropertyMetadataOptions.AffectsRender
-                )
-            );
-
-        public static readonly DependencyProperty TimeResolutionProperty =
-            DependencyProperty.Register(
-                "TimeResolution", typeof(int), typeof(MusicalGrid),
-                new FrameworkPropertyMetadata(
-                    480,
-                    FrameworkPropertyMetadataOptions.AffectsRender
-                )
-            );
-
-        public static readonly DependencyProperty ZoomFactorProperty =
-            DependencyProperty.Register(
-                "ZoomFactor", typeof(int), typeof(MusicalGrid),
-                new FrameworkPropertyMetadata(
-                    24,
-                    FrameworkPropertyMetadataOptions.AffectsRender
-                )
-            );
-
         private static readonly DependencyPropertyKey ContentBoundaryKey =
             DependencyProperty.RegisterReadOnly(
                 "ContentBoundary", typeof(Rect), typeof(MusicalGrid),
                 new FrameworkPropertyMetadata(
                     new Rect(),
-                    FrameworkPropertyMetadataOptions.AffectsRender
-                )
-            );
-
-        public static readonly DependencyProperty GridUnitProperty =
-            DependencyProperty.Register(
-                "GridUnit", typeof(int), typeof(MusicalGrid),
-                new FrameworkPropertyMetadata(
-                    16,
                     FrameworkPropertyMetadataOptions.AffectsRender
                 )
             );
@@ -91,37 +46,13 @@ namespace JUMO.UI.Controls
 
         #region Properties
 
-        public int Numerator
-        {
-            get => (int)GetValue(NumeratorProperty);
-            set => SetValue(NumeratorProperty, value);
-        }
-
-        public int Denominator
-        {
-            get => (int)GetValue(DenominatorProperty);
-            set => SetValue(DenominatorProperty, value);
-        }
-
-        public int TimeResolution
-        {
-            get => (int)GetValue(TimeResolutionProperty);
-            set => SetValue(TimeResolutionProperty, value);
-        }
-
-        public int ZoomFactor
-        {
-            get => (int)GetValue(ZoomFactorProperty);
-            set => SetValue(ZoomFactorProperty, value);
-        }
+        private int Numerator => MusicalProps.GetNumerator(this);
+        private int Denominator => MusicalProps.GetDenominator(this);
+        private int TimeResolution => MusicalProps.GetTimeResolution(this);
+        private int ZoomFactor => MusicalProps.GetZoomFactor(this);
+        private int GridUnit => MusicalProps.GetGridUnit(this);
 
         public Rect ContentBoundary => (Rect)GetValue(ContentBoundaryProperty);
-
-        public int GridUnit
-        {
-            get => (int)GetValue(GridUnitProperty);
-            set => SetValue(GridUnitProperty, value);
-        }
 
         public bool ShouldDrawHorizontalGrid
         {
@@ -136,9 +67,7 @@ namespace JUMO.UI.Controls
         }
 
         private int TicksPerGridUnit => (int)(TimeResolution / (GridUnit / 4.0));
-
         private double GridWidth => ZoomFactor * (16.0 / GridUnit);
-
         private double BeatWidth => ZoomFactor * (16.0 / Denominator);
 
         #endregion
