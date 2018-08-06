e1f80931fcd8d806cd374c139cf840743af3d176
Dalgona <dalgona@hontou.moe>
2018-08-06 19:20:34 +0900

    패턴 창 프로토타입 ViewModel 추가

---
+++ JUMO.UI/PrototypeChannelRackViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JUMO.Vst;
+
+namespace JUMO.UI
+{
+    class PrototypeChannelRackViewModel
+    {
+        public IEnumerable<Plugin> Plugins { get; } = PluginManager.Instance.Plugins;
+    }
+}
