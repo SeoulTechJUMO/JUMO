c3bb464f2c5e61779d2b65933d43585314b2e093
Dalgona <dalgona@hontou.moe>
2018-08-17 20:22:30 +0900

    Pattern에서 Scores를 인덱서로 캡슐화

--- JUMO/Pattern.cs
+++ JUMO/Pattern.cs
@@ -12,20 +12,39 @@ namespace JUMO
     /// </summary>
     public class Pattern
     {
+        private IDictionary<Plugin, IEnumerable<Note>> _scores { get; } = new Dictionary<Plugin, IEnumerable<Note>>();
+
         /// <summary>
         /// 패턴의 이름을 가져오거나 설정합니다.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// 각각의 VST 플러그인에 대한 악보 정보를 저장하는 Dictionary를 가져옵니다.
-        /// </summary>
-        public IDictionary<Plugin, IEnumerable<Note>> Scores { get; } = new Dictionary<Plugin, IEnumerable<Note>>();
-
-        /// <summary>
         /// 패턴의 길이를 가져오거나 설정합니다. PPQN에 의한 상대적인 단위를 사용합니다.
         /// </summary>
         public long Length { get; set; }
+
+        /// <summary>
+        /// 인덱스로 지정된 VST 플러그인에 대응하는 악보를 나타내는 컬렉션을 가져옵니다.
+        /// </summary>
+        /// <param name="p">VST 플러그인</param>
+        public IEnumerable<Note> this[Plugin p]
+        {
+            get
+            {
+                if (_scores.TryGetValue(p, out IEnumerable<Note> score))
+                {
+                    return score;
+                }
+                else
+                {
+                    IEnumerable<Note> newScore = new List<Note>();
+                    _scores.Add(p, newScore);
+
+                    return newScore;
+                }
+            }
+        }
 
         /// <summary>
         /// 새로운 Pattern 인스턴스를 생성합니다.
