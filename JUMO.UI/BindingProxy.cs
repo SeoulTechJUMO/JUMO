f0c09cf1e839bf90fda96b9fb7dc85cb7ec716c4
Dalgona <dalgona@hontou.moe>
2018-08-22 10:35:01 +0900

    BindingProxy 클래스 추가

---
+++ JUMO.UI/BindingProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JUMO.UI
+{
+    public class BindingProxy : Freezable
+    {
+        public static DependencyProperty DataProperty =
+            DependencyProperty.Register(
+                "Data", typeof(object), typeof(BindingProxy),
+                new FrameworkPropertyMetadata(null)
+            );
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        protected override Freezable CreateInstanceCore() => new BindingProxy();
+    }
+}
