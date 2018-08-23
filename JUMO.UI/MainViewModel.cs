d9048fe5330b13e482592581605a502e472a38e2
Dalgona <dalgona@hontou.moe>
2018-08-23 17:10:22 +0900

    메인 창 레이아웃 가져옴

---
+++ JUMO.UI/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUMO.UI
+{
+    class MainViewModel : ViewModelBase
+    {
+        public override string DisplayName => "TODO";
+    }
+}
