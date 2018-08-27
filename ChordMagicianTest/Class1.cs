50aa270bf5af6f25c1cba9a63a62a77108a52fb0
WonHyun <coy4728@naver.com>
2018-08-27 16:14:15 +0900

    파일추가

---
+++ ChordMagicianTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ChordMagicianTest
+{
+    public class Class1
+    {
+        static void jsonParser()
+        {
+            var json = new JObject();
+            json.Add("id", "Luna");
+            json.Add("name", "Silver");
+            json.Add("age", 19);
+            Console.WriteLine(json.ToString());
+        }
+    }
+}
