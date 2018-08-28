2587722d8f19649dc713d5040f16caa0dcd38242
Dalgona <dalgona@hontou.moe>
2018-08-28 14:24:35 +0900

    IMusicalViewCallback 인터페이스 정의

---
+++ JUMO.UI/Controls/IMusicalViewCallback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUMO.UI.Controls
+{
+    interface IMusicalViewCallback
+    {
+        void MusicalViewResizing(object musicalObject, double delta);
+        void MusicalViewResizeComplete(object musicalObject);
+    }
+}
