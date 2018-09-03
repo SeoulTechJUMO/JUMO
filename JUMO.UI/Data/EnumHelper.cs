b2ef30b93253aff4f3491e062fa91537a81fde57
Dalgona <dalgona@hontou.moe>
2018-09-03 09:52:19 +0900

    EnumHelper static 클래스 추가
    IEnumerable<T>에 갖다 붙일 수 있는 확장 메서드

---
+++ JUMO.UI/Data/EnumHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUMO.UI.Data
+{
+    static class EnumHelper
+    {
+        public static (T min, T max) MinMaxBy<T, U>(this IEnumerable<T> ts, Func<T, U> mapper) where U : IComparable<U>
+        {
+            T min, max;
+
+            min = max = ts.First();
+
+            foreach (T item in ts)
+            {
+                min = mapper(min).CompareTo(mapper(item)) < 0 ? min : item;
+                max = mapper(max).CompareTo(mapper(item)) > 0 ? max : item;
+            }
+
+            return (min, max);
+        }
+    }
+}
