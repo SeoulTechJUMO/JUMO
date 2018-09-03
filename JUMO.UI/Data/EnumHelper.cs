e014a5e39834c947bef6ee537471872d7f28e0a2
Dalgona <dalgona@hontou.moe>
2018-09-03 10:19:03 +0900

    EnumHelper.{Min,Max}By 확장 메서드 추가

--- JUMO.UI/Data/EnumHelper.cs
+++ JUMO.UI/Data/EnumHelper.cs
@@ -8,6 +8,30 @@ namespace JUMO.UI.Data
 {
     static class EnumHelper
     {
+        public static T MinBy<T, U>(this IEnumerable<T> ts, Func<T, U> mapper) where U : IComparable<U>
+        {
+            T min = ts.First();
+
+            foreach (T item in ts)
+            {
+                min = mapper(min).CompareTo(mapper(item)) < 0 ? min : item;
+            }
+
+            return min;
+        }
+
+        public static T MaxBy<T, U>(this IEnumerable<T> ts, Func<T, U> mapper) where U : IComparable<U>
+        {
+            T max = ts.First();
+
+            foreach (T item in ts)
+            {
+                max = mapper(max).CompareTo(mapper(item)) > 0 ? max : item;
+            }
+
+            return max;
+        }
+
         public static (T min, T max) MinMaxBy<T, U>(this IEnumerable<T> ts, Func<T, U> mapper) where U : IComparable<U>
         {
             T min, max;
