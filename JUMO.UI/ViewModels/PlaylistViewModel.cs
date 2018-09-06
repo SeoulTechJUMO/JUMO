664fbe7f54e940e80e6622e44e243c2fcf0fbf6d
Dalgona <dalgona@hontou.moe>
2018-09-06 19:09:26 +0900

    PlaylistView 틀 잡기
    PianoRollView랑 거의 비슷한 구조

--- JUMO.UI/ViewModels/PlaylistViewModel.cs
+++ JUMO.UI/ViewModels/PlaylistViewModel.cs
@@ -8,8 +8,84 @@ namespace JUMO.UI
 {
     public class PlaylistViewModel : WorkspaceViewModel
     {
-        public override WorkspaceKey Key => PlaylistWorkspaceKey.Instance;
+        private const double ZOOM_BASE = 4.0;
+        private const int ZOOM_PERCENT_MIN = 25;
+        private const int ZOOM_PERCENT_MAX = 400;
 
+        private int _zoomPercent = 100;
+        private int _gridStep = 1;
+        private bool _snapToGrid = true;
+
+        private RelayCommand _zoomInCommand;
+        private RelayCommand _zoomOutCommand;
+        private RelayCommand _resetZoomCommand;
+
+        public override WorkspaceKey Key => PlaylistWorkspaceKey.Instance;
         public override string DisplayName { get; } = "플레이리스트";
+
+        public int Numerator => Song.Current.Numerator;
+        public int Denominator => Song.Current.Denominator;
+        public int TimeResolution => Song.Current.TimeResolution;
+
+        public int ZoomFactor { get; set; } = 4;
+
+        public int ZoomPercent
+        {
+            get => _zoomPercent;
+            private set
+            {
+                _zoomPercent = Math.Max(ZOOM_PERCENT_MIN, Math.Min(value, ZOOM_PERCENT_MAX));
+                ZoomFactor = (int)(ZOOM_BASE * _zoomPercent / 100.0);
+
+                OnPropertyChanged(nameof(ZoomPercent));
+                OnPropertyChanged(nameof(ZoomFactor));
+            }
+        }
+
+        public IEnumerable<int> GridStepOptions { get; } = new[] { 1, 2, 4 };
+
+        public int GridStep
+        {
+            get => _gridStep;
+            set
+            {
+                _gridStep = value;
+                OnPropertyChanged(nameof(GridStep));
+            }
+        }
+
+        public bool SnapToGrid
+        {
+            get => _snapToGrid;
+            set
+            {
+                _snapToGrid = value;
+                OnPropertyChanged(nameof(SnapToGrid));
+            }
+        }
+
+        public RelayCommand ZoomInCommand
+        {
+            get => _zoomInCommand ?? (_zoomInCommand = new RelayCommand(
+                    _ => ZoomPercent += ZoomPercent >= 100 ? 10 : 5,
+                    _ => ZoomPercent < ZOOM_PERCENT_MAX
+                ));
+        }
+
+        public RelayCommand ZoomOutCommand
+        {
+            get => _zoomOutCommand ?? (_zoomOutCommand = new RelayCommand(
+                    _ => ZoomPercent -= ZoomPercent > 100 ? 10 : 5,
+                    _ => ZoomPercent > ZOOM_PERCENT_MIN
+                ));
+        }
+
+        public RelayCommand ResetZoomCommand
+        {
+            get => _resetZoomCommand ?? (_resetZoomCommand = new RelayCommand(
+                    _ => ZoomPercent = 100,
+                    _ => ZoomPercent != 100
+                ));
+        }
     }
 }
---
+++ JUMO.UI/Views/PlaylistView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JUMO.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for PlaylistView.xaml
+    /// </summary>
+    public partial class PlaylistView : UserControl
+    {
+        public PlaylistView()
+        {
+            InitializeComponent();
+        }
+
+        private void MusicalCanvas_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                PlaylistViewModel vm = (PlaylistViewModel)DataContext;
+                RelayCommand zoomCmd = e.Delta > 0 ? vm.ZoomInCommand : vm.ZoomOutCommand;
+
+                if (zoomCmd.CanExecute(null))
+                {
+                    zoomCmd.Execute(null);
+                }
+
+                e.Handled = true;
+            }
+        }
+    }
+}
