9d5088b41664676903f73a1a877417a3bc6842b6
Dalgona <dalgona@hontou.moe>
2018-09-06 13:51:12 +0900

    Score에 NotePropertyChanged 이벤트 추가
    악보 내의 어느 음표의 속성이 변경되었을 때 발생

--- JUMO.Core/Score.cs
+++ JUMO.Core/Score.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace JUMO
 {
@@ -7,9 +9,37 @@ namespace JUMO
     {
         public Pattern Pattern { get; }
 
+        public event EventHandler NotePropertyChanged;
+
         public Score(Pattern pattern)
         {
             Pattern = pattern ?? throw new ArgumentNullException(nameof(pattern));
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+            {
+                foreach (Note note in e.OldItems)
+                {
+                    note.PropertyChanged -= OnNotePropertyChanged;
+                }
+            }
+
+            if (e.NewItems != null)
+            {
+                foreach (Note note in e.NewItems)
+                {
+                    note.PropertyChanged += OnNotePropertyChanged;
+                }
+            }
+
+            base.OnCollectionChanged(e);
+        }
+
+        private void OnNotePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            NotePropertyChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 }
