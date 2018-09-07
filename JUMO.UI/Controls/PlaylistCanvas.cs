f845b8cd04db70d675d23772c8683ad564ea8d76
Dalgona <dalgona@hontou.moe>
2018-09-07 12:42:41 +0900

    PlaylistCanvas 컨트롤 클래스 추가

---
+++ JUMO.UI/Controls/PlaylistCanvas.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JUMO.UI.Controls
+{
+    class PlaylistCanvas : MusicalCanvasBase, IMusicalViewCallback
+    {
+        #region Dependency Properties
+
+        public static DependencyProperty SnapToGridProperty =
+            DependencyProperty.Register(
+                "SnapToGrid", typeof(bool), typeof(PlaylistCanvas),
+                new FrameworkPropertyMetadata(true)
+            );
+
+        public bool SnapToGrid
+        {
+            get => (bool)GetValue(SnapToGridProperty);
+            set => SetValue(SnapToGridProperty, value);
+        }
+
+        #endregion
+
+        #region MusicalCanvasBase Members
+
+        protected override double CalculateLogicalLength()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Rect CalculateRectForElement(FrameworkElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Size CalculateSizeForElement(FrameworkElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IVirtualElement CreateVirtualElementForItem(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IMusicalViewCallback Members
+
+        public void MusicalViewLeftButtonDown(FrameworkElement view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewMoveComplete(FrameworkElement view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewMoveStarted(FrameworkElement view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewMoving(FrameworkElement view, double deltaX, double deltaY)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewResizeComplete(FrameworkElement view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewResizeStarted(FrameworkElement view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewResizing(FrameworkElement view, double delta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MusicalViewRightButtonDown(FrameworkElement view)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
