d98744345320a03ced1c648985b10d1b9be315e7
Dalgona <dalgona@hontou.moe>
2018-09-07 14:52:42 +0900

    Score 클래스에 주석 문서 추가

--- JUMO.Core/Score.cs
+++ JUMO.Core/Score.cs
@@ -6,12 +6,26 @@ using System.ComponentModel;
 
 namespace JUMO
 {
+    /// <summary>
+    /// 음표(Note)를 배치할 수 있는 악보를 나타냅니다.
+    /// </summary>
     public class Score : ObservableCollection<Note>
     {
         private long _length = 0;
 
+        /// <summary>
+        /// 악보에 배치된 임의의 음표의 속성이 변경되었을 때 발생하는 이벤트입니다.
+        /// </summary>
+        public event EventHandler NotePropertyChanged;
+
+        /// <summary>
+        /// 이 악보가 속해 있는 패턴의 인스턴스를 가져옵니다.
+        /// </summary>
         public Pattern Pattern { get; }
 
+        /// <summary>
+        /// 이 악보의 길이를 가져옵니다. PPQN에 의한 상대적인 단위를 사용합니다.
+        /// </summary>
         public long Length
         {
             get => _length;
@@ -25,8 +39,10 @@ namespace JUMO
             }
         }
 
-        public event EventHandler NotePropertyChanged;
-
+        /// <summary>
+        /// 새로운 Score 인스턴스를 생성합니다.
+        /// </summary>
+        /// <param name="pattern">이 악보가 속하게 될 패턴. null은 사용할 수 없습니다.</param>
         public Score(Pattern pattern)
         {
             Pattern = pattern ?? throw new ArgumentNullException(nameof(pattern));
