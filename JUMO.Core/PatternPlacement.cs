c4815dee9e1efc0a5d954f3faa7d25ffd84af3a0
Dalgona <dalgona@hontou.moe>
2018-09-10 11:47:58 +0900

    PatternPlacement, Track 수정
    PatternPlacement: TrackIndex 속성 추가
    Track: Add를 사용하지 못하게 하고 PlacePattern으로 대체

--- JUMO.Core/PatternPlacement.cs
+++ JUMO.Core/PatternPlacement.cs
@@ -12,12 +12,31 @@ namespace JUMO
     /// </summary>
     public class PatternPlacement : INotifyPropertyChanged
     {
+        private int _trackIndex;
         private long _start;
 
         /// <summary>
         /// 배치된 패턴 인스턴스를 가져옵니다.
         /// </summary>
         public Pattern Pattern { get; }
+
+        public int TrackIndex
+        {
+            get => _trackIndex;
+            internal set
+            {
+                if (_trackIndex != value)
+                {
+                    if (_trackIndex < 0 || _trackIndex >= Song.NumOfTracks)
+                    {
+                        throw new IndexOutOfRangeException($"{nameof(TrackIndex)} must be in [0, {Song.NumOfTracks})");
+                    }
+
+                    _trackIndex = value;
+                    OnPropertyChanged(nameof(TrackIndex));
+                }
+            }
+        }
 
         /// <summary>
         /// 배치된 패턴의 시작 시점을 가져오거나 설정합니다. PPQN에 의한 상대적인 단위를 사용합니다.
@@ -46,10 +65,12 @@ namespace JUMO
         /// 새로운 PatternPlacement 인스턴스를 생성합니다.
         /// </summary>
         /// <param name="pattern">배치된 패턴</param>
+        /// <param name="trackIndex">패턴이 배치된 트랙의 인덱스</param>
         /// <param name="start">배치된 패턴의 시작 지점 (PPQN 기반)</param>
-        public PatternPlacement(Pattern pattern, long start)
+        internal PatternPlacement(Pattern pattern, int trackIndex, long start)
         {
             Pattern = pattern ?? throw new ArgumentNullException(nameof(pattern));
+            TrackIndex = trackIndex;
             Start = start;
 
             pattern.PropertyChanged += OnPatternPropertyChanged;
