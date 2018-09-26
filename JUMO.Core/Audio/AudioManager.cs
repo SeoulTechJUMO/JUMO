ecaaacbb24e2df013c2bd3a4f5915492ecd7901f
Dalgona <dalgona@hontou.moe>
2018-09-26 23:25:37 +0900

    오디오 출력 관련 코드를 JUMO.Core로 이동

---
+++ JUMO.Core/Audio/AudioManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using NAudio.Wave;
+
+namespace JUMO.Audio
+{
+    public sealed class AudioManager : INotifyPropertyChanged
+    {
+        #region Singleton
+
+        private static readonly Lazy<AudioManager> _instance = new Lazy<AudioManager>(() => new AudioManager());
+
+        public static AudioManager Instance => _instance.Value;
+
+        private AudioManager() => PopulateAudioOutputDevices();
+
+        #endregion
+
+        private readonly List<AudioOutputDevice> _outputDevices = new List<AudioOutputDevice>();
+        private AudioOutputDevice _currentOutputDevice = null;
+        private AudioOutputEngine _outputEngine = null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler OutputDeviceChanged;
+
+        public IEnumerable<AudioOutputDevice> OutputDevices
+        {
+            get
+            {
+                foreach (AudioOutputDevice device in _outputDevices)
+                {
+                    yield return device;
+                }
+            }
+        }
+
+        public AudioOutputDevice CurrentOutputDevice
+        {
+            get => _currentOutputDevice;
+            set
+            {
+                if (!Equals(_currentOutputDevice, value))
+                {
+                    _currentOutputDevice = value;
+                    _outputEngine?.Dispose();
+                    _outputEngine = value == null ? null : new AudioOutputEngine(value);
+
+                    OnPropertyChanged(nameof(CurrentOutputDevice));
+                    OutputDeviceChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public void AddMixerInput(ISampleProvider input)
+            => _outputEngine?.AddMixerInput(input);
+
+        private void PopulateAudioOutputDevices()
+        {
+            int numOfWaveOutDevices = WaveOut.DeviceCount;
+
+            for (int i = 0; i < numOfWaveOutDevices; i++)
+            {
+                _outputDevices.Add(new WaveOutDevice(i));
+            }
+
+            foreach (var dsDev in DirectSoundOut.Devices)
+            {
+                _outputDevices.Add(new DirectSoundOutputDevice(dsDev.Guid, dsDev.Description));
+            }
+
+            foreach (var asio in AsioOut.GetDriverNames())
+            {
+                _outputDevices.Add(new AsioOutputDevice(asio));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ JUMO.Core/Audio/AudioOutputEngine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace JUMO.Audio
+{
+    public class AudioOutputEngine : IDisposable
+    {
+        private const int SAMPLE_RATE = 44100;
+        private const int NUM_CHANNEL = 2;
+
+        private readonly IWavePlayer outputDevice;
+        private readonly MixingSampleProvider mixer;
+
+        public AudioOutputEngine(AudioOutputDevice device)
+        {
+            System.Diagnostics.Debug.WriteLine($"Initializing a new audio output engine with {device.FriendlyName}");
+            switch (device.Type)
+            {
+                case AudioOutputDeviceType.WaveOut:
+                    outputDevice = new WaveOut() { DeviceNumber = (int)device.Identifier };
+                    break;
+                case AudioOutputDeviceType.DirectSound:
+                    outputDevice = new DirectSoundOut((Guid)device.Identifier);
+                    break;
+                case AudioOutputDeviceType.Asio:
+                    outputDevice = new AsioOut((string)device.Identifier);
+                    break;
+            }
+
+            mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(SAMPLE_RATE, NUM_CHANNEL))
+            {
+                ReadFully = true
+            };
+
+            outputDevice.Init(mixer);
+            outputDevice.Play();
+        }
+
+        public void AddMixerInput(ISampleProvider input)
+        {
+            mixer.AddMixerInput(input);
+        }
+
+        public void Dispose()
+        {
+            System.Diagnostics.Debug.WriteLine("Disposing the current audio output engine");
+            outputDevice.Stop();
+            outputDevice.Dispose();
+        }
+    }
+}
