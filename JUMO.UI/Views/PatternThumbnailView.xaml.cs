9ba63ca8cb32e141502f3f615a58a0478fa44bc7
Dalgona <dalgona@hontou.moe>
2018-09-29 20:01:03 +0900

    플레이리스트에서 배치된 패턴의 thumbnail 표시
    여러가지 최적화 및 시스템 개선 필요

---
+++ JUMO.UI/Views/PatternThumbnailView.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JUMO.UI.Views
+{
+    public partial class PatternThumbnailView : UserControl
+    {
+        #region Dependency Properties
+
+        public static readonly DependencyProperty PatternProperty =
+            DependencyProperty.Register(
+                "Pattern", typeof(Pattern), typeof(PatternThumbnailView),
+                new FrameworkPropertyMetadata(PatternPropertyChangedCallback)
+            );
+
+        #endregion
+
+        #region Properties
+
+        public Pattern Pattern
+        {
+            get => (Pattern)GetValue(PatternProperty);
+            set => SetValue(PatternProperty, value);
+        }
+
+        #endregion
+
+        private readonly GeometryGroup _geometry = new GeometryGroup() { FillRule = FillRule.Nonzero };
+
+        public PatternThumbnailView()
+        {
+            InitializeComponent();
+        }
+
+        #region Callbacks
+
+        private void OnPatternPropertyChanged(Pattern oldPattern, Pattern newPattern)
+        {
+            GeometryCollection gc = _geometry.Children;
+            Rect bounds = Rect.Empty;
+
+            gc.Clear();
+
+            foreach (Vst.Plugin plugin in Vst.PluginManager.Instance.Plugins)
+            {
+                GeometryGroup geometry = ScoreThumbnailManager.Instance[Pattern[plugin]];
+                bounds.Union(geometry.Bounds);
+
+                gc.Add(geometry);
+            }
+
+            bounds.Width = Pattern.Length;
+            contentBrush.Viewbox = bounds;
+            thumbnailPath.Data = _geometry;
+        }
+
+        private static void PatternPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((PatternThumbnailView)d).OnPatternPropertyChanged(e.OldValue as Pattern, e.NewValue as Pattern);
+        }
+
+        #endregion
+    }
+}
