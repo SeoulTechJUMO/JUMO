ac2ca8fe04b9c9aad82ef54b51865878faa21cad
Dalgona <dalgona@hontou.moe>
2018-10-03 09:48:15 +0900

    [WIP] MasterSequencer 클래스 추가

---
+++ JUMO.Core/Playback/MasterSequencer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MidiToolkit = Sanford.Multimedia.Midi;
+
+namespace JUMO.Playback
+{
+    enum PlaybackMode
+    {
+        Pattern,
+        Song
+    }
+
+    class MasterSequencer : INotifyPropertyChanged, IDisposable
+    {
+        private readonly MidiToolkit.MidiInternalClock _clock = new MidiToolkit.MidiInternalClock();
+
+        private bool _isDisposed = false;
+        private long _position = 0;
+
+        #region Properties
+
+        public int TimeResolution
+        {
+            get
+            {
+                if (_isDisposed)
+                {
+                    throw new ObjectDisposedException(nameof(MasterSequencer));
+                }
+
+                return _clock.Ppqn;
+            }
+            set
+            {
+                if (_isDisposed)
+                {
+                    throw new ObjectDisposedException(nameof(MasterSequencer));
+                }
+
+                _clock.Ppqn = value;
+            }
+        }
+
+        public int MidiTempo
+        {
+            get
+            {
+                if (_isDisposed)
+                {
+                    throw new ObjectDisposedException(nameof(MasterSequencer));
+                }
+
+                return _clock.Tempo;
+            }
+            set
+            {
+                if (_isDisposed)
+                {
+                    throw new ObjectDisposedException(nameof(MasterSequencer));
+                }
+
+                _clock.Tempo = value;
+            }
+        }
+
+        public long Position
+        {
+            get => _position;
+            set
+            {
+                if (_isDisposed)
+                {
+                    throw new ObjectDisposedException(nameof(MasterSequencer));
+                }
+
+                throw new NotImplementedException();
+
+                OnPropertyChanged(nameof(Position));
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler Tick
+        {
+            add => _clock.Tick += value;
+            remove => _clock.Tick -= value;
+        }
+
+        #endregion
+
+        public MasterSequencer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            if (_isDisposed)
+            {
+                throw new ObjectDisposedException(nameof(MasterSequencer));
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public void Continue()
+        {
+            if (_isDisposed)
+            {
+                throw new ObjectDisposedException(nameof(MasterSequencer));
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            if (_isDisposed)
+            {
+                throw new ObjectDisposedException(nameof(MasterSequencer));
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #region IDisposable Support
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _clock.Dispose();
+            }
+
+            _isDisposed = true;
+        }
+
+        public void Dispose() => Dispose(true);
+
+        #endregion
+    }
+}
