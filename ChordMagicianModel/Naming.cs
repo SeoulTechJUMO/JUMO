394eb88aff634c36d287366cfd6a6659621fe2b2
Dalgona <dalgona@hontou.moe>
2018-10-08 20:49:04 +0900

    음계를 나타내는 리스트를 배열로 변환

--- ChordMagicianModel/Naming.cs
+++ ChordMagicianModel/Naming.cs
@@ -14,57 +14,35 @@ namespace ChordMagicianModel
             { 0, "C" }, { 1, "C#" }, { 2, "D" }, { 3, "D#" }, { 4, "E" }, { 5, "F" }, { 6, "F#" }, { 7, "G" }, { 8, "G#" }, { 9, "A" }, { 10, "A#" }, { 11, "B" }
         };
 
-        public static readonly List<byte> Major = new List<byte>()
-        {
-            0, 2, 4, 5, 7, 9, 11
-        };
+        public static readonly byte[] Major = new byte[] { 0, 2, 4, 5, 7, 9, 11 };
 
-        public static readonly List<byte> Minor = new List<byte>()
-        {
-            0, 2, 3, 5, 7, 8, 10
-        };
+        public static readonly byte[] Minor = new byte[] { 0, 2, 3, 5, 7, 8, 10 };
 
-        public static readonly List<byte> Lydian = new List<byte>()
-        {
-            0, 2, 4, 6, 7, 9, 11
-        };
+        public static readonly byte[] Lydian = new byte[] { 0, 2, 4, 6, 7, 9, 11 };
 
-        public static readonly List<byte> Mixolydian = new List<byte>()
-        {
-            0, 2, 4, 5, 7, 9, 10
-        };
+        public static readonly byte[] Mixolydian = new byte[] { 0, 2, 4, 5, 7, 9, 10 };
 
-        public static readonly List<byte> Dorian = new List<byte>()
-        {
-            0, 2, 3, 5, 7, 9, 10
-        };
+        public static readonly byte[] Dorian = new byte[] { 0, 2, 3, 5, 7, 9, 10 };
 
-        public static readonly List<byte> Phrygian = new List<byte>()
-        {
-            0, 1, 3, 5, 7, 8, 10
-        };
+        public static readonly byte[] Phrygian = new byte[] { 0, 1, 3, 5, 7, 8, 10 };
 
-        public static readonly List<byte> Locrian = new List<byte>()
-        {
-            0, 1, 3, 5, 6, 8, 10
-        };
+        public static readonly byte[] Locrian = new byte[] { 0, 1, 3, 5, 6, 8, 10 };
 
-        public static readonly Dictionary<string, List<byte>> ShowMode = new Dictionary<string, List<byte>>()
+        public static readonly Dictionary<string, byte[]> ShowMode = new Dictionary<string, byte[]>()
         {
             { "Major", Major }, { "Minor", Minor }
         };
 
-        public static readonly Dictionary<string, List<byte>> Scale = new Dictionary<string, List<byte>>()
+        public static readonly Dictionary<string, byte[]> Scale = new Dictionary<string, byte[]>()
         {
             { "Major", Major }, { "Minor", Minor }, { "L" , Lydian }, { "M", Mixolydian }, { "D", Dorian }, { "Y", Phrygian }, { "C", Locrian }
         };
 
         public static List<byte> CalScale(string key, string mode)
         {
-            List<byte> scale = Scale[mode];
             List<byte> _scale = new List<byte>();
 
-            foreach (byte i in scale)
+            foreach (byte i in Scale[mode])
             {
                 _scale.Add((byte)((i + NoteName[key]) % 12));
             }
@@ -72,7 +50,7 @@ namespace ChordMagicianModel
             return _scale;
         }
 
-        public static List<byte> CalScale(string key, List<byte> scale)
+        public static List<byte> CalScale(string key, byte[] scale)
         {
             List<byte> _scale = new List<byte>();
 
