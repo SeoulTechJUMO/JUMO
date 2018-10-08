185b9ce9f9df702c83684f984f09c99b10500665
WonHyun <coy4728@naver.com>
2018-10-08 19:11:55 +0900

    재수정

--- ChordMagicianModel/CreateMelody.cs
+++ ChordMagicianModel/CreateMelody.cs
@@ -10,17 +10,17 @@ namespace ChordMagicianModel
 {
     public class CreateMelody
     {
-        static readonly string StartUp = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
-        static readonly string MelodyPath = StartUp + "/ChordMagicianModel/Melody ";
-
         //JUMO 프로젝트 안에 마젠타를 포함시킨 경우
         public void RunMagenta(string Progress, int NumOfFiles)
         {
+            string StartUp = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+            string MelodyPath = StartUp + "/ChordMagicianModel/Melody ";
+
             string FileName = StartUp + "/improv_rnn_generate/improv_rnn_generate.exe";
             string Args = "--config=chord_pitches_improv " +
                 "--bundle_file=" + StartUp + "/improv_rnn_generate/chord_pitches_improv.mag " +
                 "--output_dir=" + MelodyPath +
-                "--num_outputs=" + NumOfFiles +
+                "--num_outputs=" + NumOfFiles + " " +
                 "--backing_chords=\"" + Progress + "\" "+
                 //이부분은 추후 삭제
                 "--render_chords";
@@ -28,7 +28,7 @@ namespace ChordMagicianModel
             //이미 생성된 멜로디 파일이 있다면 지워준다
             if (System.IO.Directory.Exists(MelodyPath))
             {
-                string[] files = System.IO.Directory.GetFiles(MelodyPath);
+                string[] files = GetMelodyPath();
 
                 foreach (string s in files)
                 {
@@ -37,6 +37,16 @@ namespace ChordMagicianModel
             }
 
             RunProcess(FileName, Args);
+        }
+
+        //생성된 미디파일 경로 얻기
+        public string[] GetMelodyPath()
+        {
+            string StartUp = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+            string MelodyPath = StartUp + "/ChordMagicianModel/Melody ";
+
+            string[] files = System.IO.Directory.GetFiles(MelodyPath);
+            return files;
         }
 
         public int RunProcess(String FileName, String Args)
