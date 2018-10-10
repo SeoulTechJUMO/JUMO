0de4c83e0ec631e71ea5b04578c02fdc552550b3
WonHyun <coy4728@naver.com>
2018-10-10 18:59:12 +0900

    믹서채널분리

---
+++ JUMO.UI/ViewModels/MixerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUMO.UI.ViewModels
+{
+    public class MixerViewModel : ViewModelBase
+    {
+        public override string DisplayName => "믹서";
+
+        //믹서 채널
+
+    }
+}
