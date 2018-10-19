918b8cdd50d3a59fc574e71eab0ef2e256d3768f
Eunbin Jeong <dmsqls7353@gmail.com>
2018-10-19 18:48:51 +0900

    스킨 리소스 추가. 기본+버튼 스타일 정의

---
+++ JUMO.UI/ToggleButtonIndicator.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace JUMO.UI
+{
+    static class ToggleButtonIndicator
+    {
+        public static readonly DependencyProperty BrushProperty =
+            DependencyProperty.RegisterAttached(
+                "Brush", typeof(Brush), typeof(ToggleButtonIndicator),
+                new FrameworkPropertyMetadata(Brushes.White, FrameworkPropertyMetadataOptions.AffectsRender)
+            );
+
+        public static Brush GetBrush(UIElement target) => (Brush)target.GetValue(BrushProperty);
+        public static void SetBrush(UIElement target, Brush value) => target.SetValue(BrushProperty, value);
+    }
+}
