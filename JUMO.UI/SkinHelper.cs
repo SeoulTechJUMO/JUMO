0a7eb897c977c4290826a9ce6997915307f8e81d
Eunbin Jeong <dmsqls7353@gmail.com>
2018-10-23 14:58:18 +0900

    Segmented control (왼쪽, 가운데, 오른쪽) 구현

--- JUMO.UI/SkinHelper.cs
+++ JUMO.UI/SkinHelper.cs
@@ -3,6 +3,14 @@ using System.Windows.Media;
 
 namespace JUMO.UI
 {
+    enum SegmentOption
+    {
+        None,
+        Left,
+        Center,
+        Right
+    }
+
     static class SkinHelper
     {
         public static readonly DependencyProperty IndicatorBrushProperty =
@@ -11,7 +19,25 @@ namespace JUMO.UI
                 new FrameworkPropertyMetadata(Brushes.White, FrameworkPropertyMetadataOptions.AffectsRender)
             );
 
+        public static readonly DependencyProperty BorderRadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "BorderRadius", typeof(CornerRadius), typeof(SkinHelper),
+                new FrameworkPropertyMetadata(new CornerRadius(2.0), FrameworkPropertyMetadataOptions.AffectsRender)
+            );
+
+        public static readonly DependencyProperty SegmentProperty =
+            DependencyProperty.RegisterAttached(
+                "Segment", typeof(SegmentOption), typeof(SkinHelper),
+                new FrameworkPropertyMetadata(SegmentOption.None, FrameworkPropertyMetadataOptions.AffectsRender)
+            );
+
         public static Brush GetIndicatorBrush(UIElement target) => (Brush)target.GetValue(IndicatorBrushProperty);
         public static void SetIndicatorBrush(UIElement target, Brush value) => target.SetValue(IndicatorBrushProperty, value);
+
+        public static CornerRadius GetBorderRadius(UIElement target) => (CornerRadius)target.GetValue(BorderRadiusProperty);
+        public static void SetBorderRadius(UIElement target, CornerRadius value) => target.SetValue(BorderRadiusProperty, value);
+
+        public static SegmentOption GetSegment(UIElement target) => (SegmentOption)target.GetValue(SegmentProperty);
+        public static void SetSegment(UIElement target, SegmentOption value) => target.SetValue(SegmentProperty, value);
     }
 }
