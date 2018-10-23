1b63a9e3a46c9f306cf847e9f142cd1bd12fbf1c
Eunbin Jeong <dmsqls7353@gmail.com>
2018-10-23 13:48:17 +0900

    이름 변경: ToggleButtonIndicator -> SkinHelper
    Brush -> IndicatorBrush

---
+++ JUMO.UI/SkinHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace JUMO.UI
+{
+    static class SkinHelper
+    {
+        public static readonly DependencyProperty IndicatorBrushProperty =
+            DependencyProperty.RegisterAttached(
+                "IndicatorBrush", typeof(Brush), typeof(SkinHelper),
+                new FrameworkPropertyMetadata(Brushes.White, FrameworkPropertyMetadataOptions.AffectsRender)
+            );
+
+        public static Brush GetIndicatorBrush(UIElement target) => (Brush)target.GetValue(IndicatorBrushProperty);
+        public static void SetIndicatorBrush(UIElement target, Brush value) => target.SetValue(IndicatorBrushProperty, value);
+    }
+}
