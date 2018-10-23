feeebed2f43221f8566570cb150ca6a0ae2ba14b
Eunbin Jeong <dmsqls7353@gmail.com>
2018-10-23 11:04:07 +0900

    스킨 테스트 창 여는 버튼 임시로 추가

--- JUMO.UI/Layouts/MainWindow.xaml.cs
+++ JUMO.UI/Layouts/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace JUMO.UI.Layouts
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new SkinTestWindow().Show();
+        }
     }
 }
