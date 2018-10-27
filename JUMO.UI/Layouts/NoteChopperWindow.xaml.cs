733e19bd8a97dbb14430de8c083ca510240d181e
WonHyun <coy4728@naver.com>
2018-10-27 14:28:02 +0900

    Window cancel 버튼에도 초기화적용

--- JUMO.UI/Layouts/NoteChopperWindow.xaml.cs
+++ JUMO.UI/Layouts/NoteChopperWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using JUMO.UI.ViewModels;
 
 namespace JUMO.UI.Layouts
 {
@@ -22,6 +11,11 @@ namespace JUMO.UI.Layouts
         public NoteChopperWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            ((NoteToolsViewModel)DataContext).Abort();
         }
 
         private void CheckButtonClick(object sender, RoutedEventArgs e)
--- JUMO.UI/Layouts/NoteSofterWindow.xaml.cs
+++ JUMO.UI/Layouts/NoteSofterWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using JUMO.UI.ViewModels;
 
 namespace JUMO.UI.Layouts
 {
@@ -24,6 +13,11 @@ namespace JUMO.UI.Layouts
             InitializeComponent();
         }
 
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            ((NoteToolsViewModel)DataContext).Abort();
+        }
+
         private void CheckButtonClick(object sender, RoutedEventArgs e)
         {
             Close();
@@ -33,5 +27,7 @@ namespace JUMO.UI.Layouts
         {
             Close();
         }
+
+        
     }
 }
