57f09eef3d21093069c289e7e58751594fe4be34
Dalgona <dalgona@hontou.moe>
2018-11-02 03:23:30 +0900

    Magenta가 실행될 때 사용하는 경로 수정

--- ChordMagicianModel/CreateMelody.cs
+++ ChordMagicianModel/CreateMelody.cs
@@ -6,8 +6,8 @@ namespace ChordMagicianModel
 {
     public static class CreateMelody
     {
-        private static readonly string _startup = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
-        private static readonly string _melodyPath = _startup + "/ChordMagicianModel/Melody ";
+        private static readonly string _startup = Path.Combine(Environment.CurrentDirectory, "tools", "improv_rnn_generate");
+        private static readonly string _melodyPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "JUMO", "Melody");
 
         //생성된 미디파일 경로 얻기
         public static string[] MelodyPath => Directory.GetFiles(_melodyPath);
@@ -15,12 +15,12 @@ namespace ChordMagicianModel
         //JUMO 프로젝트 안에 마젠타를 포함시킨 경우
         public static void RunMagenta(string progress, int numOfFiles)
         {
-            string fileName = _startup + "/improv_rnn_generate/improv_rnn_generate.exe";
+            string fileName = Path.Combine(_startup, "improv_rnn_generate.exe");
 
             string args =
                 "--config=chord_pitches_improv " +
-                $"--bundle_file={_startup}/improv_rnn_generate/chord_pitches_improv.mag " +
-                $"--output_dir={_melodyPath} " +
+                $"--bundle_file=\"{Path.Combine(_startup, "chord_pitches_improv.mag")}\" " +
+                $"--output_dir=\"{_melodyPath}\" " +
                 $"--num_outputs={numOfFiles} " +
                 $"--backing_chords=\"{progress}\" " +
                 "--render_chords";
@@ -39,7 +39,7 @@ namespace ChordMagicianModel
             RunProcess(fileName, args);
         }
 
-        private static int RunProcess(String fileName, String args)
+        private static int RunProcess(string fileName, string args)
         {
             Process p = new Process();
 
