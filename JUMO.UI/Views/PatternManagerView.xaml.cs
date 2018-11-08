7bf8fe62974ee97098a9ef8b3f98caff2c082791
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-08 13:28:27 +0900

    메인 창에서 PatternManagerView 분리

---
+++ JUMO.UI/Views/PatternManagerView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace JUMO.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for PatternManagerView.xaml
+    /// </summary>
+    public partial class PatternManagerView : UserControl
+    {
+        public PatternManagerView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPopupOpened(object sender, EventArgs e)
+        {
+            nameTextBox.Focus();
+            nameTextBox.SelectAll();
+        }
+    }
+}
