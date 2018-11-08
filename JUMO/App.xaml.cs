ea60fb427913f339de0820e97b0ec1070db05278
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-08 19:37:12 +0900

    시작 화면 표시. 시작하는 동안 핵심 부분 초기화

--- JUMO/App.xaml.cs
+++ JUMO/App.xaml.cs
@@ -1,9 +1,35 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using JUMO.UI.Layouts;
 
 namespace JUMO
 {
     public partial class App : Application
     {
+        private volatile Audio.AudioManager audioManager;
+        private volatile Vst.PluginManager pluginManager;
+        private volatile Playback.MasterSequencer masterSequencer;
+        private volatile MixerManager mixerManager;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            SplashWindow splashWindow = new SplashWindow();
+
+            splashWindow.Show();
+
+            Task loadTask = PrepareApp();
+            Task delayTask = Task.Delay(3000);
+
+            await loadTask;
+            await delayTask;
+
+            mainWindow.Show();
+            splashWindow.Close();
+
+            base.OnStartup(e);
+        }
+
         protected override void OnExit(ExitEventArgs e)
         {
             Audio.AudioManager.Instance.CurrentOutputDevice = null;
@@ -11,6 +37,17 @@ namespace JUMO
             Playback.MasterSequencer.Instance.Dispose();
 
             base.OnExit(e);
+        }
+
+        private async Task PrepareApp()
+        {
+            await Task.Run(() =>
+            {
+                Dispatcher.Invoke(() => audioManager = Audio.AudioManager.Instance);
+                Dispatcher.Invoke(() => pluginManager = Vst.PluginManager.Instance);
+                Dispatcher.Invoke(() => masterSequencer = Playback.MasterSequencer.Instance);
+                Dispatcher.Invoke(() => mixerManager = MixerManager.Instance);
+            });
         }
     }
 }
