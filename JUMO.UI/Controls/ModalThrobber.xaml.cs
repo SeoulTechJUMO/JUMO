db131766c4d4b8b982f9a568894641cce4f42c66
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-13 17:24:38 +0900

    플러그인 로드 중 메시지를 ModalThrobber 컨트롤로 분리

---
+++ JUMO.UI/Controls/ModalThrobber.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace JUMO.UI.Controls
+{
+    public partial class ModalThrobber : ContentControl
+    {
+        public ModalThrobber()
+        {
+            InitializeComponent();
+        }
+    }
+}
