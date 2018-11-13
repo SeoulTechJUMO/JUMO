f6ca3067205a9915f0a6e3cf798271ada52ec088
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-13 17:32:56 +0900

    ModalThrobber.IsActive 속성 추가

--- JUMO.UI/Controls/ModalThrobber.xaml.cs
+++ JUMO.UI/Controls/ModalThrobber.xaml.cs
@@ -1,9 +1,21 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace JUMO.UI.Controls
 {
     public partial class ModalThrobber : ContentControl
     {
+        public static DependencyProperty IsActiveProperty =
+            DependencyProperty.Register(
+                nameof(IsActive), typeof(bool), typeof(ModalThrobber), new FrameworkPropertyMetadata(false)
+            );
+
+        public bool IsActive
+        {
+            get => (bool)GetValue(IsActiveProperty);
+            set => SetValue(IsActiveProperty, value);
+        }
+
         public ModalThrobber()
         {
             InitializeComponent();
