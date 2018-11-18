df3eac4cb3d3b781aedf3001b681bed7f4ac5b5c
Dalgona <dalgona@hontou.moe>
2018-11-18 13:57:45 +0900

    노트 편집 도구 ViewModel 파일 분리

---
+++ JUMO.UI/ViewModels/ChopperViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+
+namespace JUMO.UI.ViewModels
+{
+    public class ChopperViewModel : NoteToolsViewModel
+    {
+        public override string DisplayName => "자르기";
+
+        public ChopperViewModel(PianoRollViewModel vm) : base(vm)
+        {
+            _ChopInterval = 0;
+            addCount = 0;
+        }
+
+        private int addCount;
+
+        private int _ChopInterval;
+        public int ChopInterval
+        {
+            get => _ChopInterval;
+            set
+            {
+                if (value != _ChopInterval)
+                {
+                    if (value >= 0 && value <= 16 && value % 2 == 0)
+                    {
+                        _ChopInterval = value;
+                        Chopping(value);
+                        OnPropertyChanged(nameof(ChopInterval));
+                    }
+                }
+            }
+        }
+
+        private void Chopping(int interval)
+        {
+            int currentChopLength = 0;
+            if (interval != 0) { currentChopLength = Song.Current.TimeResolution / interval; }
+
+            for (int i = 0; i < addCount; i++)
+            {
+                ViewModel.RemoveNote(ViewModel.Notes?.ElementAt(ViewModel.Notes.Count - 1).Source);
+            }
+
+            addCount = 0;
+
+            if (currentChopLength == 0)
+            {
+                Reset();
+            }
+            else
+            {
+                for (int i = 0; i < OrderedNotes.Count(); i++)
+                {
+                    for (int j = 0; j < OrderedNotes[i].Count(); j++)
+                    {
+                        int firstLength = 0;
+                        while (true)
+                        {
+                            if ((OrderedNotes[i][j].Start + firstLength) % currentChopLength == 0)
+                            {
+                                if (firstLength != 0)
+                                {
+                                    break;
+                                }
+                            }
+                            firstLength++;
+                        }
+                        OrderedNotes[i][j].Length = firstLength;
+
+                        int lengthHandle = OriginalNotes[i][j].Length - firstLength;
+                        int startHandle = OriginalNotes[i][j].Start + firstLength;
+                        while(lengthHandle > currentChopLength)
+                        {
+                            ViewModel.AddNote(new Note(OriginalNotes[i][j].Value, OriginalNotes[i][j].Velocity, startHandle, currentChopLength));
+                            addCount++;
+                            lengthHandle -= currentChopLength;
+                            startHandle += currentChopLength;
+                        }
+                        if (lengthHandle != 0) { ViewModel.AddNote(new Note(OriginalNotes[i][j].Value, OriginalNotes[i][j].Velocity, startHandle, lengthHandle)); addCount++; }
+                    }
+                }
+            }
+        }
+
+        public void ChopperReset()
+        {
+            Chopping(0);
+        }
+    }
+}
---
+++ JUMO.UI/ViewModels/SofterViewModel.cs
@@ -0,0 +1,218 @@
+﻿using System.Linq;
+
+namespace JUMO.UI.ViewModels
+{
+    public class SofterViewModel : NoteToolsViewModel
+    {
+        public override string DisplayName => "부드럽게";
+
+        public SofterViewModel(PianoRollViewModel vm) : base(vm)
+        {
+            _StartInterval = 0.0;
+            _VelocityInterval = 0.0;
+            _LengthInterval = 0.0;
+            _StartAdjustRange = 30;
+            _VelocityAdjustRange = 30;
+            _LengthAdjustRange = 30;
+        }
+
+        #region Attributes
+
+        private int _StartAdjustRange;
+        private int _VelocityAdjustRange;
+        private int _LengthAdjustRange;
+
+        private double _StartInterval;
+        private double _VelocityInterval;
+        private double _LengthInterval;
+
+        #endregion
+
+        #region Property
+
+        public int StartAdjustRange
+        {
+            get => _StartAdjustRange;
+            set
+            {
+                if (0 <= value && value <= 100)
+                {
+                    _StartAdjustRange = value;
+                    AdjustStart(StartInterval);
+                    OnPropertyChanged(nameof(StartAdjustRange));
+                }
+            }
+        }
+        public int VelocityAdjustRange
+        {
+            get => _VelocityAdjustRange;
+            set
+            {
+                if (0 <= value && value <= 100)
+                {
+                    _VelocityAdjustRange = value;
+                    AdjustVelocity(VelocityInterval);
+                    OnPropertyChanged(nameof(VelocityAdjustRange));
+                }
+            }
+        }
+        public int LengthAdjustRange
+        {
+            get => _LengthAdjustRange;
+            set
+            {
+                if (0 <= value && value <= 100)
+                {
+                    _LengthAdjustRange = value;
+                    AdjustLength(LengthInterval);
+                    OnPropertyChanged(nameof(LengthAdjustRange));
+                }
+            }
+        }
+
+        public double StartInterval
+        {
+            get => _StartInterval;
+            set
+            {
+                if (-1.0 <= value && value <= 1.0)
+                {
+                    _StartInterval = value;
+                    AdjustStart(value);
+                    OnPropertyChanged(nameof(StartInterval));
+                }
+            }
+        }
+        public double VelocityInterval
+        {
+            get => _VelocityInterval;
+            set
+            {
+                if (-1.0 <= value && value <= 1.0)
+                {
+                    _VelocityInterval = value;
+                    AdjustVelocity(value);
+                    OnPropertyChanged(nameof(VelocityInterval));
+                }
+            }
+        }
+        public double LengthInterval
+        {
+            get => _LengthInterval;
+            set
+            {
+                if (-1.0 <= value && value <= 1.0)
+                {
+                    _LengthInterval = value;
+                    AdjustLength(value);
+                    OnPropertyChanged(nameof(LengthInterval));
+                }
+            }
+        }
+
+        #endregion
+
+        private void AdjustStart(double interval)
+        {
+            bool IsDesc = false;
+            if (interval < 0) { IsDesc = true; interval = - (interval); }
+            int startDelta = 0;
+            int delta = (int)(interval * StartAdjustRange);
+
+            for(int i=0;i<OrderedNotes.Count();i++)
+            {
+                startDelta = 0;
+                if (IsDesc)
+                {
+                    for(int j = OrderedNotes[i].Count() - 1; j >= 0; j--)
+                    {
+                        OrderedNotes[i][j].Start = OriginalNotes[i][j].Start + startDelta;
+                        if (OrderedNotes[i][j].Start < OriginalNotes[i][j].Start)
+                        {
+                            OrderedNotes[i][j].Start = OriginalNotes[i][j].Start;
+                        }
+                        startDelta += delta;
+                        OrderedNotes[i][j].UpdateSource();
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < OrderedNotes[i].Count(); j++)
+                    {
+                        OrderedNotes[i][j].Start = OriginalNotes[i][j].Start + startDelta;
+                        if (OrderedNotes[i][j].Start < OriginalNotes[i][j].Start)
+                        {
+                            OrderedNotes[i][j].Start = OriginalNotes[i][j].Start;
+                        }
+                        startDelta += delta;
+                        OrderedNotes[i][j].UpdateSource();
+                    }
+                }
+            }
+        }
+        private void AdjustVelocity(double interval)
+        {
+            bool IsDesc = false;
+            if (interval < 0) { IsDesc = true; interval = -(interval); }
+            int veloDelta = 0;
+            int delta = (int)(interval * VelocityAdjustRange);
+
+            for (int i = 0; i < OrderedNotes.Count(); i++)
+            {
+                veloDelta = 0;
+                if (IsDesc)
+                {
+                    for (int j = OrderedNotes[i].Count() - 1; j >= 0; j--)
+                    {
+                        OrderedNotes[i][j].Velocity = (byte)(OriginalNotes[i][j].Velocity - veloDelta);
+                        if (OrderedNotes[i][j].Velocity > 127) { OrderedNotes[i][j].Velocity = 0; }
+                        veloDelta += delta;
+                        OrderedNotes[i][j].UpdateSource();
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < OrderedNotes[i].Count(); j++)
+                    {
+                        OrderedNotes[i][j].Velocity = (byte)(OriginalNotes[i][j].Velocity - veloDelta);
+                        if (OrderedNotes[i][j].Velocity > 127) { OrderedNotes[i][j].Velocity = 0; }
+                        veloDelta += delta;
+                        OrderedNotes[i][j].UpdateSource();
+                    }
+                }
+            }
+        }
+        private void AdjustLength(double interval)
+        {
+            bool IsDesc = false;
+            if (interval < 0) { IsDesc = true; interval = -(interval); }
+            int lenDelta = 0;
+            int delta = (int)(interval * LengthAdjustRange);
+
+            for (int i = 0; i < OrderedNotes.Count(); i++)
+            {
+                lenDelta = 0;
+                if (IsDesc)
+                {
+                    for (int j = OrderedNotes[i].Count() - 1; j >= 0; j--)
+                    {
+                        if (OriginalNotes[i][j].Length - lenDelta < 10) { OrderedNotes[i][j].Length = 10; }
+                        else { OrderedNotes[i][j].Length = OriginalNotes[i][j].Length - lenDelta; }
+                        lenDelta += delta;
+                        OrderedNotes[i][j].UpdateSource();
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < OrderedNotes[i].Count(); j++)
+                    {
+                        if (OriginalNotes[i][j].Length - lenDelta < 10) { OrderedNotes[i][j].Length = 10; }
+                        else { OrderedNotes[i][j].Length = OriginalNotes[i][j].Length - lenDelta; }
+                        lenDelta += delta;
+                        OrderedNotes[i][j].UpdateSource();
+                    }
+                }
+            }
+        }
+    }
+}
