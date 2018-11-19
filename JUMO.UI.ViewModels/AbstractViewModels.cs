fab2865a7d5487cb2bf8781c1395c031e506a74b
Dalgona <dalgona@hontou.moe>
2018-11-19 23:00:50 +0900

    ViewModelBase 클래스를 JUMO.UI.Core 어셈블리로 이동

--- JUMO.UI.ViewModels/AbstractViewModels.cs
+++ JUMO.UI.ViewModels/AbstractViewModels.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace JUMO.UI.ViewModels
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public abstract string DisplayName { get; }
-
-        protected void OnPropertyChanged(string propertyName)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
     public abstract class WorkspaceViewModel : ViewModelBase
     {
         private RelayCommand _closeCommand;
