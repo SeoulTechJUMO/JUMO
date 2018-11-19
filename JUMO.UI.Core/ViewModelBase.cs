6b586749325d466df9aec5349285940fea915f0f
Dalgona <dalgona@hontou.moe>
2018-11-19 23:08:40 +0900

    CloseCommand를 ViewModelBase로 이동

--- JUMO.UI.Core/ViewModelBase.cs
+++ JUMO.UI.Core/ViewModelBase.cs
@@ -1,12 +1,23 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace JUMO.UI
 {
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        private RelayCommand _closeCommand;
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler CloseRequested;
 
         public abstract string DisplayName { get; }
+
+        public RelayCommand CloseCommand
+            => _closeCommand ?? (_closeCommand = new RelayCommand(ExecuteClose, CanExecuteClose));
+
+        protected virtual void ExecuteClose() => CloseRequested?.Invoke(this, EventArgs.Empty);
+
+        protected virtual bool CanExecuteClose() => true;
 
         protected void OnPropertyChanged(string propertyName)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- JUMO.UI.ViewModels/AbstractViewModels.cs
+++ JUMO.UI.ViewModels/AbstractViewModels.cs
@@ -1,16 +1,8 @@
-﻿using System;
-
-namespace JUMO.UI.ViewModels
+﻿namespace JUMO.UI.ViewModels
 {
     public abstract class WorkspaceViewModel : ViewModelBase
     {
-        private RelayCommand _closeCommand;
-
-        public event EventHandler CloseRequested;
-
         internal abstract WorkspaceKey Key { get; }
-
-        public RelayCommand CloseCommand => _closeCommand ?? (_closeCommand = new RelayCommand(_ => CloseRequested?.Invoke(this, EventArgs.Empty)));
     }
 
     public abstract class SettingsGroupViewModel : ViewModelBase
